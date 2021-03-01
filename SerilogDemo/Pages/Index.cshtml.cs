afe4c2b932484105b81c6ac1c3a1557b8c2544e6
dattq <dattq@metechvn.com>
2021-03-01 16:20:50 +0700

    add configuration log

--- SerilogDemo/Pages/Index.cshtml.cs
+++ SerilogDemo/Pages/Index.cshtml.cs
@@ -19,6 +19,24 @@ namespace SerilogDemo.Pages
 
         public void OnGet()
         {
+            _logger.LogInformation("You requested the Index page.");
+
+            try
+            {
+                for (var i = 0; i < 100; i++)
+                {
+                    if (i == 56)
+                    {
+                        throw new Exception("This is our demo exception");
+                    }
+
+                    _logger.LogInformation("The value of i is {LoopCountValue}", i);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "We caught this exception in the Index Get call.");
+            }
         }
     }
 }
--- SerilogDemo/Startup.cs
+++ SerilogDemo/Startup.cs
@@ -1,13 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace SerilogDemo
 {
@@ -43,6 +39,8 @@ namespace SerilogDemo
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
+            app.UseSerilogRequestLogging();
+            
             app.UseRouting();
 
             app.UseAuthorization();
